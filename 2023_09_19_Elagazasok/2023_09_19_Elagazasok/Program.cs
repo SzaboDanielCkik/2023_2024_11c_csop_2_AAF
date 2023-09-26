245f76c28983a9f139550606df5949e6a1594605
Szabó Dániel <szabo.daniel@ckik.hu>
2023-09-26 08:45:39 +0200

    2023_09_26

--- 2023_09_19_Elagazasok/2023_09_19_Elagazasok/Program.cs
+++ 2023_09_19_Elagazasok/2023_09_19_Elagazasok/Program.cs
@@ -71,6 +71,53 @@ namespace _2023_09_19_Elagazasok
             }
             // HF pdf-ből 7-9.
 
+            // HF pdf-ből 10-13 
+
+            // Generálj ki két véletlen számot [-10,10] között. Szorozd össze a két értéket. Ha a szorzat kiesik az intervallumból, akkor ezt jelezzük a felhasználónak. Ha az intervallumon belül van, akkor írassuk ki a szorzat értékét!
+
+            Random r = new Random();
+            int a = r.Next(-10, 11);
+            int b = r.Next(-10, 11);
+            int szorzat = a * b;
+            Console.WriteLine(a + " " + b);
+            if (szorzat >= -10 && szorzat <= 10)
+            {
+                Console.WriteLine("A szorzat: " + szorzat);
+            }
+            else
+            {
+                Console.WriteLine("A szorzat nincs benne az intervallumba.");
+            }
+
+            // Az ékezet betűket szeretném lecserélni. Kérjen be a felhasználótól egy ékezetes betűt és írassa ki ékezet nélkül! Ha nem jó betűt adott meg, jelezze a felhasználónak!
+            //á é í ó ö ő ú ü ű
+            Console.Write("Adjon meg egy ékezetes betűt: ");
+            string betu = Console.ReadLine();
+            switch (betu)
+            {
+                case "á": Console.WriteLine("a"); break;
+                case "é": Console.WriteLine("e"); break;
+                case "í": Console.WriteLine("i"); break;
+                case "ó": Console.WriteLine("o"); break;
+                case "ö": Console.WriteLine("o"); break;
+                case "ő": Console.WriteLine("o"); break;
+                case "ú": Console.WriteLine("u"); break;
+                case "ü": Console.WriteLine("u"); break;
+                case "ű": Console.WriteLine("u"); break;
+                default: Console.WriteLine("Nem ékezetes betűt adott meg!"); break;
+            }
+
+            // HF 11-12 - Farenheit - C
+
+            double F = 126;
+            double C = (F-32)/1.8;
+            Console.WriteLine("{0} F° = {1} C°", F, C);
+
+            C = 52.4;
+            F = C * 1.8 + 32;
+            Console.WriteLine("{0} C° = {1} F°", C, F);
+
+
             Console.ReadLine();
         }
     }
---
+++ 2023_09_21_Ciklusok/2023_09_21_Ciklusok/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_09_21_Ciklusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ciklusok
+             * Számlálós
+             * 
+             * ciklus i = kezdőértéktől vég értékig léptetés
+             *      Ciklus mag
+             * ciklus vége
+             * 
+             * for(int i = kezdőérték; i<végérték; i+=léptetés)
+             * {// Egy utasítás esetén a zárójel elhagyható.
+             *      Ciklusmag
+             * }
+             * 
+             * Elől tesztelős
+             * ciklus amíg(benmaradási feltétel(ek))
+             *      Ciklus mag;
+             * ciklus vége
+             * 
+             * while(benmaradási feltétel(ek))
+             * {
+             *      Ciklus mag;
+             * }
+             * 
+             * Hátul tesztelős
+             * 
+             * Csináld
+             *      Ciklus mag;
+             * amíg(benmaradási feltétel(ek));
+             * 
+             * do{
+             *      Ciklus mag;
+             * }while(benmaradási feltétel(ek));*/
+
+            // Irasd ki az első 30 pozitív páros számot!
+            for (int i = 2; i < 61; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            // i++; i = i+1; i += 1;
+            for (int i = 1; i < 31; i++)
+            {
+                Console.Write(i*2 + " ");
+            }
+            Console.WriteLine();
+            //Írass ki 15 db véletlen számot [50, 1000] között úgy, hogy a szám 10-zel osztható legyen!
+
+            Random r = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write(r.Next(5, 101) * 10 + " ");
+            }
+            Console.WriteLine();
+
+            int db = 1;
+            do {
+                int vel = r.Next(50, 1001);
+                if (vel % 10 == 0)
+                {
+                    db++;
+                    Console.Write(vel + " ");
+                }
+            } while (db<=15);
+            Console.WriteLine();
+            // Generálj ki véletlen számokat [50,1000] addig 
+            // amíg 7-tel oszthatót nem kapunk!
+
+            int a;
+            do
+            {
+                a = r.Next(50, 1001);
+                Console.Write(a + " ");
+            }
+            while (a % 7 != 0);
+
+
+            int szam = r.Next(50, 1001);
+            while (szam % 7 != 0)
+            {
+                Console.Write(szam + " ");
+                szam = r.Next(50, 1001);
+            }
+            Console.WriteLine(szam);
+
+            // 26-28
+
+            Console.ReadLine();
+        }
+    }
+}
