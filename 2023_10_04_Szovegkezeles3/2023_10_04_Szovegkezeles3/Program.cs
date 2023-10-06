7e51a208c9245e6098aa31ad3329db6507302689
Szabó Dániel <szabo.daniel@ckik.hu>
2023-10-06 14:16:52 +0200

    2023_10_06

---
+++ 2023_10_04_Szovegkezeles3/2023_10_04_Szovegkezeles3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_10_04_Szovegkezeles3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HF 14-25 feladatok
+
+            // Kérjen be a felhasználótól egy dupla betűt. (Hossza = 2)
+            // Ha felhasználó nem két betűt adott meg, akkor addig kérje be a program, amíg jól nem adja meg.
+            // Adott egy szöveg, és döntse el, hogy van-e a szövegben ez a dupla betű!
+            // szöveg: Senki sem tökéletes de a programozók átkozottúl közel állnak hozzá.
+
+
+            Console.ReadLine();
+        }
+    }
+}
