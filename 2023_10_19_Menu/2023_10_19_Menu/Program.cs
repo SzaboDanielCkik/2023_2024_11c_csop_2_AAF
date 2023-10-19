d76af290fff526b4e8aa47c76a273f7470d6cce1
Szabó Dániel <szabo.daniel@ckik.hu>
2023-10-19 09:42:06 +0200

    2023_10_19

---
+++ 2023_10_19_Menu/2023_10_19_Menu/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_10_19_Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. - Két szám megadása");
+                Console.WriteLine("2. - A két szám összege");
+                Console.WriteLine("3. - A két szám szorzata");
+                Console.WriteLine("0. - Kilépés");
+                Console.Write("Adjon meg egy menüpontot: ");
+                menu = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine("be: két szám");
+                        break;
+                    case 2:
+                        Console.WriteLine("összeg");
+                        break;
+                    case 3:
+                        Console.WriteLine("szorzat");
+                        break;
+                    case 0:
+                        Console.WriteLine("kilépés");
+                        break;
+                    default:
+                        Console.WriteLine();
+                        break;
+                }
+                Console.ReadLine();
+            } while (menu != 0);
+        }
+    }
+}
