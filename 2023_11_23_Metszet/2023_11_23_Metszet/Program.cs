efc8a9eb2421c4bdaa85515d91ad65c582e84e7e
Szabó Dániel <szabo.daniel@ckik.loc>
2023-11-30 08:13:32 +0100

    2023_11_30

--- 2023_11_23_Metszet/2023_11_23_Metszet/Program.cs
+++ 2023_11_23_Metszet/2023_11_23_Metszet/Program.cs
@@ -11,6 +11,7 @@ namespace _2023_11_23_Metszet
         static int[] tomb = new int[13];
         static int[] tomb2 = new int[17];
         static int[] metszet;
+        static int[] unio;
         static Random r = new Random();
         static void Main(string[] args)
         {
@@ -20,26 +21,51 @@ namespace _2023_11_23_Metszet
             TombKiiratas(tomb2);
             Metszet(tomb, tomb2);
             TombKiiratas(metszet);
+            Unio(tomb, tomb2);
+            TombKiiratas(unio);
 
             Console.ReadLine();
         }
 
+        static void Unio(int[] tomb1, int[] tomb2)
+        {
+            unio = new int[tomb1.Length+tomb2.Length];
+            for (int i = 0; i < tomb1.Length; i++)
+                unio[i] = tomb1[i];
+            int k = tomb1.Length;
+            for (int i = 0; i < tomb2.Length; i++)
+            {
+                if (!BenneVanE(unio, tomb2[i]))
+                {
+                    unio[k] = tomb2[i];
+                    k++;
+                }
+            }
+        }
+
         static void Metszet(int[] tomb1, int[] tomb2)
         {
+            // Létrehozunk egy tömböt, aminek a hossza a kisebb tömb hosszával egyenlő
             int n = 0;
             if (tomb1.Length < tomb2.Length)
                 n = tomb1.Length;
             else
                 n = tomb2.Length;
             metszet = new int[n];
+            // Metszet - a közös elemek kiszedése
             int k = 0;
             for (int i = 0; i < tomb1.Length; i++)
             {
                 int a = tomb1[i];
-                int j = 0;
+                /*int j = 0;
                 while (j < tomb2.Length && tomb2[j] != a)
                     j++;
                 if (j < tomb2.Length)
+                {
+                    metszet[k] = a;
+                    k++;
+                }*/
+                if (BenneVanE(tomb2, a))
                 {
                     metszet[k] = a;
                     k++;
@@ -72,6 +98,18 @@ namespace _2023_11_23_Metszet
                     k++;
                 }
             }
+        }
+
+        static bool BenneVanE(int[] tomb, int a)
+        {
+            int i = 0;
+            while (i < tomb.Length && tomb[i] != a)
+            {
+                i++;
+            }
+            bool vane = i < tomb.Length;
+            return vane;
+            //return i<tomb.Length;
         }
     }
 }
---
+++ 2023_11_30_Gyakorlas/2023_11_30_Gyakorlas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_11_30_Gyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Eljárás: Töltsön fel két 23 elemű tömböt [10,50] között úgy, hogy minden számból csak egy szerepelhet!
+             * Feladat1 E: Adja meg mindkét tömb (FV) mediánját!
+             * Feladat2 E: Melyek azok a páros számok, amelyek mindkét tömbben szerepelnek?
+             */
+
+            /*Tombfeltoltes(tomb1);
+            Tombfeltoltes(tomb2);
+            TombKiiratas(tomb1);
+            TombKiiratas(tomb2);
+            Feladat1();
+            Feladat2();*/
+
+            Console.ReadLine();
+        }
+    }
+}
