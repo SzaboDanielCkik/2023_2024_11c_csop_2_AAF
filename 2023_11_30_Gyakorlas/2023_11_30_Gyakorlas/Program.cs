9639924eabf5d95d3515d314382d30eb8d6475b3
Szabó Dániel <szabo.daniel@ckik.loc>
2023-12-07 09:33:36 +0100

    2023_12_07

--- 2023_11_30_Gyakorlas/2023_11_30_Gyakorlas/Program.cs
+++ 2023_11_30_Gyakorlas/2023_11_30_Gyakorlas/Program.cs
@@ -21,20 +21,61 @@ namespace _2023_11_30_Gyakorlas
 
             TombFeltoltes(tomb1);
             TombFeltoltes(tomb2);
-            TombKiiratas(tomb1);
-            TombKiiratas(tomb2);
+            TombKiiratas(tomb1, tomb1.Length);
+            TombKiiratas(tomb2,tomb2.Length);
             Feladat1();
-            //Feladat2();
+            Feladat2();
+            Feladat3();
 
             Console.ReadLine();
+        }
+
+        static void Feladat3()
+        {
+            int[] unio = new int[46];
+            int k = 0;
+            for (int i = 0; i < tomb1.Length; i++)
+            {
+                if (tomb1[i] % 2 == 1)
+                {
+                    unio[k] = tomb1[i];
+                    k++;
+                }
+            }
+            
+            for (int i = 0; i < tomb2.Length; i++)
+            {
+                if (tomb2[i] %2 == 1 && !BenneVanE(unio, tomb2[i]))
+                {
+                    unio[k] = tomb2[i];
+                    k++;
+                }    
+            }
+            TombKiiratas(unio,k);
+        }
+
+        static void Feladat2()
+        {
+            int[] metszet = new int[23];
+            int k = 0;
+            for (int i = 0; i < tomb1.Length; i++)
+            {
+                //if (BenneVanE(tomb2, tomb1[i]) && tomb1[i] % 2 == 0)
+                if (tomb1[i] % 2 == 0 && BenneVanE(tomb2, tomb1[i]))
+                {
+                    metszet[k] = tomb1[i];
+                    k++;
+                }    
+            }
+            TombKiiratas(metszet,k);
         }
 
         static void Feladat1()
         {
             Rendezes(tomb1);
             Rendezes(tomb2);
-            TombKiiratas(tomb1);
-            TombKiiratas(tomb2);
+            TombKiiratas(tomb1,tomb1.Length);
+            TombKiiratas(tomb2,tomb2.Length);
             Console.WriteLine("Az első tömb mediánja: " + Median(tomb1));
             Console.WriteLine("A második tömb mediánja: " + Median(tomb2));
         }
@@ -68,9 +109,9 @@ namespace _2023_11_30_Gyakorlas
             }
         }
 
-        static void TombKiiratas(int[] tomb)
+        static void TombKiiratas(int[] tomb, int n)
         {
-            for (int i = 0; i < tomb.Length; i++)
+            for (int i = 0; i < n; i++)
                 Console.Write(tomb[i] + " ");
             Console.WriteLine();
         }
