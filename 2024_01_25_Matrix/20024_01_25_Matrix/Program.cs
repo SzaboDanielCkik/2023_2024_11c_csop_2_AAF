54ba4357eeb68a2bd17194a0097e8ef6908340ca
Szabó Dániel <szabo.daniel@ckik.loc>
2024-01-30 13:31:14 +0100

    2024_01_30

--- 2024_01_25_Matrix/20024_01_25_Matrix/Program.cs
+++ 2024_01_25_Matrix/20024_01_25_Matrix/Program.cs
@@ -8,7 +8,7 @@ namespace _20024_01_25_Matrix
 {
     class Program
     {
-        static int[,] tomb = new int[4, 6];
+        static int[,] tomb = new int[6, 4];
         static void Main(string[] args)
         {
             /* Mátrix - 2d Tömb - [sor, oszlop] - Típus
@@ -34,12 +34,89 @@ namespace _20024_01_25_Matrix
 
             MatrixFeltoltese(tomb);
             MatrixKiir(tomb);
-            Feladat1();
-            Feladat2();
-            Feladat3();
-            Feladat4();
+            //Feladat1();
+            //Feladat2();
+            //Feladat3();
+            //Feladat4();
+            //Feladat5();
+            //Feladat6();
+            Feladat7();
 
             Console.ReadLine();
+        }
+
+        static void Feladat7()
+        {
+            Console.WriteLine("7. feladat");
+            int[,] tokorkep = MatrixTukrozes(tomb);
+            MatrixKiir(tokorkep);
+        }
+
+        static int[,] MatrixTukrozes(int[,] matrix)
+        {
+            int eredetiSor = matrix.GetLength(0);
+            int eredetiOszlop = matrix.GetLength(1);
+            int[,] tukor = new int[eredetiOszlop , eredetiSor ];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    tukor[j,i] = matrix[i,j];
+                }
+            }
+
+            return tukor;
+        }
+
+        static void Feladat6()
+        {
+            Console.WriteLine("6. feladat: ");
+            Console.WriteLine("Az oszlopok legnagyobb értékei: ");
+            List<int> oszlopMaxok = OszlopokMaxErtekei();
+            for (int i = 0; i < oszlopMaxok.Count; i++)
+                Console.WriteLine("{0}. oszlop maximuma: {1}", i + 1, oszlopMaxok[i]);
+        }
+
+        static List<int> OszlopokMaxErtekei()
+        {
+            List<int> maximumok = new List<int>();
+            for (int i = 0; i < tomb.GetLength(1); i++)
+            {
+                int maxe = tomb[0,i];
+                for (int j = 1; j < tomb.GetLength(0); j++)
+                {
+                    if (maxe < tomb[j, i])
+                        maxe = tomb[j, i];
+                }
+                maximumok.Add(maxe);
+            }
+            return maximumok;
+        }
+
+        static void Feladat5()
+        {
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("Sorok átlaga: ");
+            List<double> sorokAtlaga = SorokAtlaga();
+            for (int i = 0; i < sorokAtlaga.Count; i++)
+                Console.WriteLine("{0}. sor átalga: {1}", i + 1, 
+                    Math.Round(sorokAtlaga[i],2));
+        }
+
+        static List<double> SorokAtlaga()
+        {
+            List<double> lista = new List<double>();
+            for (int i = 0; i < tomb.GetLength(0); i++)
+            {
+                double osszeg = 0;
+                for (int j = 0; j < tomb.GetLength(1); j++)
+                {
+                    osszeg += tomb[i, j];
+                }
+                lista.Add(osszeg / tomb.GetLength(1));
+            }
+            return lista;
         }
 
         static void Feladat4()
---
+++ 2024_01_30_MatrixGyakorlas/2024_01_30_MatrixGyakorlas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_01_30_MatrixGyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Töltsön fel egy 4x4-os mátrixot [1,20] közötti számokkal!
+             * 2. FV: Adja meg a fő és mellék átlók szorzatának hányadosát!
+             * 3. FV: Készítsen egy új mátrixot, aminek az első sorában eltárolja a sorok terjedelmét a másodikban pedig az oszlopok szórását!
+             * 4. metódus: kérjen be a felhasználótól egy sor és egy oszlop számot és írassa ki az adott helyen lévő számok szomszédját (az átlós is szomszédnak számít)*/
+
+        }
+    }
+}
