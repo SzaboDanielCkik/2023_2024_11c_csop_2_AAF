cebbd6a5392bf181c8d02790f3617cfb25437400
Szabó Dániel <szabo.daniel@ckik.loc>
2024-02-02 11:40:06 +0100

    2024_02_02

--- 2024_01_30_MatrixGyakorlas/2024_01_30_MatrixGyakorlas/Program.cs
+++ 2024_01_30_MatrixGyakorlas/2024_01_30_MatrixGyakorlas/Program.cs
@@ -19,10 +19,94 @@ namespace _2024_01_30_MatrixGyakorlas
             MatrixFeltoltes(matrix);
             MatrixKiir(matrix);
             Feladat2();
+            Feladat3();
 
             Console.ReadLine();
 
         }
+
+        static void Feladat3()
+        {
+            Console.WriteLine("3. feladat");
+            int[,] statisztika = new int[2, matrix.GetLength(0)];
+            List<int> sorokTerjedelme = SorokTerjedelme(matrix);
+            List<int> oszlopokSzorasa = OszlopokSzorasa(matrix);
+
+            for (int i = 0; i < statisztika.GetLength(1); i++)
+            {
+                statisztika[0, i] = sorokTerjedelme[i];
+                statisztika[1, i] = oszlopokSzorasa[i];
+            }
+            MatrixKiir(statisztika);
+        }
+
+        static List<int> OszlopokSzorasa(int[,] matrix)
+        {
+            List<int> szorasok = new List<int>();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                double szoras = Szoras(matrix, i);
+                szorasok.Add((int)szoras);
+            }
+            return szorasok;
+        }
+
+        static double Szoras(int[,] matrix, int oszlop)
+        {
+            double osszeg = 0;
+            double atlag = Atlag(matrix, oszlop);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                osszeg += Math.Pow(matrix[i, oszlop] - atlag, 2);
+            }
+            return Math.Sqrt(osszeg / matrix.GetLength(0));
+        }
+
+        static double Atlag(int[,] matrix, int oszlop)
+        {
+            double osszeg = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                osszeg += matrix[i, oszlop];
+            }
+            return osszeg / matrix.GetLength(0);
+        }
+
+        static List<int> SorokTerjedelme(int[,] matrix)
+        {
+            List<int> terjedelmek = new List<int>();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int terjedelem = Maximum(matrix, i) - Minimum(matrix, i);
+                terjedelmek.Add(terjedelem);
+            }
+            return terjedelmek;
+        }
+
+        static int Minimum(int[,] matrix, int sor)
+        {
+            int mine = matrix[sor, 0];
+            for (int i = 1; i < matrix.GetLength(1); i++)
+            {
+                if (mine > matrix[sor, i])
+                    mine = matrix[sor, i];
+            }
+            return mine;
+        }
+
+        static int Maximum(int[,] matrix, int sor)
+        {
+            int maxe = matrix[sor, 0];
+            for (int i = 1; i < matrix.GetLength(1); i++)
+            {
+                if (maxe < matrix[sor, i])
+                    maxe = matrix[sor, i];
+            }
+            return maxe;
+        }
+
 
         static void Feladat2()
         {
